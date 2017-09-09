9f63a5f9d8127c141eaa45683d1d3bdeb8602d00
Alex-GT <isc.agt@gmail.com>
2017-09-09 18:45:49 -0500

    Pedidos completos

---
+++ controller/pedido/delete.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+$login = $_SESSION['login'];
+$seguridad = $_SESSION['seguridad'];
+$idpedido=$_REQUEST['idpedido'];
+
+include "../../model/conexion.php";
+$objConex = new Conexion();
+$link=$objConex->conectarse();
+$sql = mysql_query("DELETE FROM pedido where idpedido='$idpedido'", $link) or die(mysql_error());
+if (!$sql){
+  die("<p>Fallo la actualización de datos: ".mysql_error()."</p>");
+}else{
+     echo  "<script type='text/javascript'>
+      window.location='../../view/pedidos.php'
+      </script>";
+    mysql_close($link);
+  }
+?>
--- view/pedidos.php
+++ view/pedidos.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="refresh" content="5" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
   <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <title></title>
@@ -20,13 +21,28 @@
     </div>
   </nav>
 <?php
+session_start();
+$login = $_SESSION['login'];
+$seguridad = $_SESSION['seguridad'];
+if (!isset($seguridad)) {
+  echo "<SCRIPT TYPE='text/javascript'>alert('Sin acceso');</SCRIPT>";
+  header('Location:../index.html');
+}
+$idrestaurante = $_SESSION['idrestaurante'];
 include '../model/conexion.php';
 
 $objConex = new Conexion();
 $link=$objConex->conectarse();
-$sql = mysql_query("SELECT * FROM pedido;", $link) or die(mysql_error());
+$sql = mysql_query("SELECT pedido.idpedido,pedido.tiposervicio,pedido.montototal,user.username
+  FROM pedido,user WHERE pedido.idrestaurante ='$idrestaurante'
+  AND pedido.idusuario = user.idusuario AND pedido.estado = 'Pendiente';", $link) or die(mysql_error());
+$sql2 = mysql_query("SELECT pedido.idpedido,pedido.tiposervicio,pedido.montototal,user.username
+  FROM pedido,user WHERE pedido.idrestaurante ='$idrestaurante'
+  AND pedido.idusuario = user.idusuario AND pedido.estado = 'En proceso';", $link) or die(mysql_error());
+$sql3 = mysql_query("SELECT pedido.idpedido,pedido.tiposervicio,pedido.montototal,user.username
+  FROM pedido,user WHERE pedido.idrestaurante ='$idrestaurante'
+  AND pedido.idusuario = user.idusuario AND pedido.estado = 'Terminado';", $link) or die(mysql_error());
 echo '
-
     <div class="row">
       <div class="col s6  card-panel ">
         <h3 class="center-align">Pendientes</h3>
@@ -44,13 +60,13 @@ echo '
             <tr>';
             while ($rows = mysql_fetch_array($sql)) {
               echo '
-
+              <td>'.$rows['username'].'</td>
               <td>'.$rows['tiposervicio'].'</td>
               <td>'.$rows['montototal'].'</td>
               <td>
-                <a  class="btn-floating waves-effect waves-light #2962ff blue accent-4"><i class="material-icons">visibility</i></a>
-                <a  class="btn-floating waves-effect waves-light green"><i class="material-icons">done</i></a>
-                <a  class="btn-floating waves-effect waves-light red"><i class="material-icons">cancel</i></a>
+                <a  class="btn-floating waves-effect waves-light #2962ff blue accent-4"href="../controller/pedido/view.php?idpedido='.$rows['idpedido'].'"><i class="material-icons">visibility</i></a>
+                <a  class="btn-floating waves-effect waves-light green"href="../controller/pedido/updateDoing.php?idpedido='.$rows['idpedido'].'"><i class="material-icons">done</i></a>
+                <a  class="btn-floating waves-effect waves-light red"href="../controller/pedido/delete.php?idpedido='.$rows['idpedido'].'"><i class="material-icons">cancel</i></a>
               </td>
             </tr>';
           }
@@ -58,60 +74,40 @@ echo '
           </tbody>
         </table class="centered striped bordered z-depth-3">
 ';
-?>
-      </div>
-      <div class="col s6  card-panel ">
-        <h3 class="center-align">Confirmados</h3>
-        <table class="striped">
-          <thead>
-            <tr>
-              <th>Nombre</th>
-              <th>Tipo de servicio</th>
-              <th>Monto total</th>
-              <th>Ver Pedido Completo</th>
-            </tr>
-          </thead>
+      echo '
+      </div>';
+      echo '
+          <div class="row">
+            <div class="col s6  card-panel ">
+              <h3 class="center-align">Confirmados</h3>
+              <table class="centered striped bordered z-depth-3">
+                <thead>
+                  <tr>
+                    <th>Nombre</th>
+                    <th>Tipo de servicio</th>
+                    <th>Monto total</th>
+                    <th>Ver Pedido Completo</th>
+                  </tr>
+                </thead>
 
-          <tbody>
-            <tr>
-              <td>Alvin</td>
-              <td>Pedido anticipado</td>
-              <td>$0.87</td>
-              <td>
-                <a  class="btn-floating waves-effect waves-light #2962ff blue accent-4"><i class="material-icons">visibility</i></a>
-                <a  class="btn-floating waves-effect waves-light green"><i class="material-icons">done_all</i></a>
-              </td>
-            </tr>
-            <tr>
-              <td>Alvin</td>
-              <td>Pedido anticipado</td>
-              <td>$0.87</td>
-              <td>
-                <a  class="btn-floating waves-effect waves-light #2962ff blue accent-4"><i class="material-icons">visibility</i></a>
-                <a  class="btn-floating waves-effect waves-light green"><i class="material-icons">done_all</i></a>
-              </td>
-            </tr>
-            <tr>
-              <td>Alvin</td>
-              <td>Pedido anticipado</td>
-              <td>$0.87</td>
-              <td>
-                <a  class="btn-floating waves-effect waves-light #2962ff blue accent-4"><i class="material-icons">visibility</i></a>
-                <a  class="btn-floating waves-effect waves-light green"><i class="material-icons">done_all</i></a>
-              </td>
-            </tr>
-            <tr>
-              <td>Alvin</td>
-              <td>Pedido anticipado</td>
-              <td>$0.87</td>
-              <td>
-                <a  class="btn-floating waves-effect waves-light #2962ff blue accent-4"><i class="material-icons">visibility</i></a>
-                <a  class="btn-floating waves-effect waves-light green"><i class="material-icons">done_all</i></a>
-              </td>
-            </tr>
-
-          </tbody>
-        </table>
+                <tbody>
+                  <tr>';
+                  while ($rows = mysql_fetch_array($sql2)) {
+                    echo '
+                    <td>'.$rows['username'].'</td>
+                    <td>'.$rows['tiposervicio'].'</td>
+                    <td>'.$rows['montototal'].'</td>
+                    <td>
+                    <a  class="btn-floating waves-effect waves-light #2962ff blue accent-4"href="../controller/pedido/view.php?idpedido='.$rows['idpedido'].'"><i class="material-icons">visibility</i></a>
+                      <a  class="btn-floating waves-effect waves-light green"href="../controller/pedido/updateDone.php?idpedido='.$rows['idpedido'].'"><i class="material-icons">done_all</i></a>
+                    </td>
+                  </tr>';
+                }
+                echo '
+                </tbody>
+              </table class="centered striped bordered z-depth-3">
+      ';
+      echo '
       </div>
     </div>
     <div class="row">
@@ -124,26 +120,30 @@ echo '
                 <th>Nombre</th>
                 <th>Tipo de servicio</th>
                 <th>Monto total</th>
+                  <th>Bye Bye</th>
               </tr>
             </thead>
 
             <tbody>
-              <tr>
-                <td>Alvin</td>
-                <td>Pedido anticipado</td>
-                <td>$0.87</td>
+              <tr>';
+              while ($rows = mysql_fetch_array($sql3)) {
+                echo '
+                <td>'.$rows['username'].'</td>
+                <td>'.$rows['tiposervicio'].'</td>
+                <td>'.$rows['montototal'].'</td>
                 <td>
-                  <i class="material-icons">thumb_up</i>
-
+                <i class="material-icons">thumb_up</i>
                 </td>
-              </tr>
+              </tr>';
+            }echo '
             </tbody>
           </table>
         </div>
       </div>
     </div>
+    ';
+?>
 
-  </form>
 </body>
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
