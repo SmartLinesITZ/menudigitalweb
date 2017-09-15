04c39baba72001beee86b2b8dfce033d5a360813
Alex-GT <isc.agt@gmail.com>
2017-09-15 17:30:10 -0500

    img DonAlex

--- view/perfil/read.php
+++ view/perfil/read.php
@@ -29,13 +29,13 @@ $row=mysql_fetch_array($sql);
     #imagen{
      height: 300px; 
      width: 300px;
-    background-repeat: no-repeat;
-    background-position: 50%;
-    border-radius: 80%;
-    background-size: 100% 100%
-    }
-    
-  </style>
+     background-repeat: no-repeat;
+     background-position: 50%;
+     border-radius: 80%;
+     background-size: 100% 100%
+   }
+
+ </style>
 </head>
 <body class="landing">
   <div id="page-wrapper">
@@ -50,50 +50,41 @@ $row=mysql_fetch_array($sql);
           <li><a href="../../index.html" class="button">Cerrar Sesión</a></li>
         </ul>
       </nav>
-      </header>
-      <section id="banner">
-        <!--<img src="<?php echo $row['logo'];?>" alt="">-->
-        <tr>
-    <td><?php echo $rows['nomcategoria']; ?>
-    <div class="row">
-    <div class="col-md-offset-1 col-md-10 col-xs-offset-1 col-xs-12">
-   <?php $i=1; while ($imagenes = @mysql_fetch_array($sql) ){ 
-   $ruta = "imagenes/" . $imagenes['nombre'];
-   ?>  
-  <div class="col-md-4 col-xs-8">
-  <div class="row">
-      <img src="<?php echo $ruta;?>">
-    </div>
-  </div>
-</div>
-  </div>
-  <?php
-  $i++; 
-  }
-  ?>
-  </div>
-  </div>
-</div>
-        <section id="main" class="container 75%">
-          <div class="box">
-            <h4 style="color: Green">Nombre de restaurte: <?php echo $row['nombrerest'];?></h4>
-            <h3 style="color: Green">Slogan: <?php echo $row['slogan'];?></h3>
-            <p style="color: Green">Descripcion: <?php echo $row['descripcion'];?> </p>
-            <p style="color: Black">Domicilio: <?php echo $row['domicilio'];?> </p>
-            <p style="color: Black">Telefono: <?php echo $row['telefono'];?></p>
-             <p style="color: Black">Horarios: <?php echo $row['horarios'];?></p>
-          </div>
-          <div class="12u" style="text-align: right;" >
-            <ul class="actions">
-              <li>
-                <a class="button special " style="background: gray; display:scroll; position:fixed; bottom:50px; right:35px;" href="update.php?idrestaurante=<?php echo $row['idrestaurante'];?>">Editar Perfil</a>
-              </li>
-            </ul>
+    </header>
+    <section id="banner">
+      <!--<img src="<?php echo $row['logo'];?>" alt="">-->
+      <div class="row">
+        <div class="col-md-offset-1 col-md-10 col-xs-offset-1 col-xs-12">
+         <?php
+         $imagen = @mysql_fetch_array($sql);
+           $ruta = "imagenes/" . $imagen['foto'];
+           ?>  
+
+           <div class="col-md-4 col-xs-8">
+            <div class="row">
+              <img src="<?php echo $ruta;?>">
+            </div>
           </div>
         </div>
-
-      </section>
-    </div>
-
-  </body>
-  </html>
+      </div>
+      <section id="main" class="container 75%">
+        <div class="box">
+          <h4 style="color: Green">Nombre de restaurte: <?php echo $row['nombrerest'];?></h4>
+          <h3 style="color: Green">Slogan: <?php echo $row['slogan'];?></h3>
+          <p style="color: Green">Descripcion: <?php echo $row['descripcion'];?> </p>
+          <p style="color: Black">Domicilio: <?php echo $row['domicilio'];?> </p>
+          <p style="color: Black">Telefono: <?php echo $row['telefono'];?></p>
+          <p style="color: Black">Horarios: <?php echo $row['horarios'];?></p>
+        </div>
+        <div class="12u" style="text-align: right;" >
+          <ul class="actions">
+            <li>
+              <a class="button special " style="background: gray; display:scroll; position:fixed; bottom:50px; right:35px;" href="update.php?idrestaurante=<?php echo $row['idrestaurante'];?>">Editar Perfil</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </div>
+</body>
+</html>
