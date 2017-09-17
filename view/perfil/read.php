6c80f0c473d8593b5fbf2974652006b0e28aad07
Alex-GT <isc.agt@gmail.com>
2017-09-17 02:04:38 -0500

    Vistas perfil

--- view/perfil/read.php
+++ view/perfil/read.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 $login = $_SESSION['login'];
@@ -15,62 +14,44 @@ $sql = mysql_query("SELECT * FROM restaurante where idrestaurante='$idrestaurant
 $row=mysql_fetch_array($sql);
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title>Login</title>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" href="../../src/assets/css/main.css" />
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <script src="../src/js/jquery.js"></script>
+  <script src="../src/js/materialize.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <style type="text/css">
-    #imagen{
-     height: 300px; 
+  <title>Perfil</title>
+</head>
+<nav class="z-depth-2 #00695c teal darken-3" role="navigation">
+  <div class="nav-wrapper container">
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="../servicios.php"><i class="material-icons left">headset_mic</i>Servicios</a></li>
+      <li><a href="../platillos/read.php"><i class="material-icons left">restaurant_menu</i>Menu de platillos</a></li>
+      <li><a href="../pedidos.php"><i class="material-icons left">room_service</i>Pedidos</a></li>
+      <li><a href="read.php"><i class="material-icons left">face</i>Perfil</a></li>
+      <li><a href="../../index.html"><i class="material-icons right">directions_run</i>Cerrar sesión</a></li>
+    </ul>
+  </div>
+</nav>
+<br><br><br>
+<body>
+  <div class="center-align">
+    <?php
+    $ruta = "../../controller/perfil/imagenes/" . $row['logo'];
+    ?>
+    <img src="<?php echo $ruta?>" class="circle" style=" height: 300px;
      width: 300px;
      background-repeat: no-repeat;
      background-position: 50%;
      border-radius: 80%;
-     background-size: 100% 100%
-   }
-
- </style>
-</head>
-<body class="landing">
-  <div id="page-wrapper">
-    <header id="header" class="alt">
-      <h1><a href="../inicio.html">Clic And Eat</a> </h1>
-      <nav id="nav">
-        <ul>
-          <li><a href="../servicios.php"><i class="material-icons">headset_mic</i> Servicios</a></li>
-          <li><a href="../platillos/read.php"><i class="material-icons">restaurant_menu</i>Menu de platillos</a></li>
-          <li><a href="../pedidos.php"> <i class="material-icons">room_service</i> Pedidos</a></li>
-          <li><a href="read.php"> <i class="material-icons">face</i>Perfil</a></li>
-          <li><a href="../../index.html" class="button">Cerrar Sesión</a></li>
-        </ul>
-      </nav>
-    </header>
-    <section id="banner">
-      <!--<img src="<?php echo $row['logo'];?>" alt="">-->
-      <div class="row">
-        <div class="col-md-offset-1 col-md-10 col-xs-offset-1 col-xs-12">
-         <?php
-         $imagen = @mysql_fetch_array($sql);
-           $ruta = "imagenes/" . $imagen['foto'];
-           ?>  
-
-           <div class="col-md-4 col-xs-8">
-            <div class="row">
-              <img src="<?php echo $ruta;?>">
-            </div>
-          </div>
-        </div>
-      </div>
-      <section id="main" class="container 75%">
-        <div class="box">
+     background-size: 100% 100%">
+    <div class="row">
+      <div class="col s8 offset-s2">
+        <div class="card ">
           <h4 style="color: Green">Nombre de restaurte: <?php echo $row['nombrerest'];?></h4>
-          <h3 style="color: Green">Slogan: <?php echo $row['slogan'];?></h3>
+          <h4 style="color: Green">Slogan: <?php echo $row['slogan'];?></h4>
           <p style="color: Green">Descripcion: <?php echo $row['descripcion'];?> </p>
           <p style="color: Black">Domicilio: <?php echo $row['domicilio'];?> </p>
           <p style="color: Black">Telefono: <?php echo $row['telefono'];?></p>
@@ -79,12 +60,10 @@ $row=mysql_fetch_array($sql);
         <div class="12u" style="text-align: right;" >
           <ul class="actions">
             <li>
-              <a class="button special " style="background: gray; display:scroll; position:fixed; bottom:50px; right:35px;" href="update.php?idrestaurante=<?php echo $row['idrestaurante'];?>">Editar Perfil</a>
+              <a class="btn-floating btn-large waves-effect waves-light" style="background: gray; display:scroll; position:fixed; bottom:50px; right:35px;" href="update.php?idrestaurante=<?php echo $row['idrestaurante'];?>"><i class="material-icons">edit</i></a>
             </li>
           </ul>
         </div>
       </div>
-    </section>
-  </div>
-</body>
-</html>
+    </body>
+    </html>
--- view/perfil/update.php
+++ view/perfil/update.php
@@ -1,99 +1,110 @@
 <?php
+session_start();
+$login = $_SESSION['login'];
+$seguridad = $_SESSION['seguridad'];
+if (!isset($seguridad)) {
+  echo "<SCRIPT TYPE='text/javascript'>alert('Sin acceso');</SCRIPT>";
+  header('Location:../../index.html');
+}
+$idrestaurante = $_SESSION['idrestaurante'];
 include "../../model/conexion.php";
 $objConex = new Conexion();
 $link=$objConex->conectarse();
-$idrestaurante=$_REQUEST['idrestaurante'];
-$sql = mysql_query("SELECT * FROM restaurante where idrestaurante='$idrestaurante' ;" , $link) or die(mysql_error());
+$sql = mysql_query("SELECT * FROM restaurante where idrestaurante='$idrestaurante';" , $link) or die(mysql_error());
 $row=mysql_fetch_array($sql);
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title>Login</title>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" href="../../src/assets/css/main.css" />
-  <link rel="stylesheet" href="http://www.anerbarrena.com/demos/2014/002-time-input-html5.php">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <script src="../src/js/jquery.js"></script>
+  <script src="../src/js/materialize.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Perfil</title>
 </head>
-<body class="landing">
-  <div id="page-wrapper">
-    <header id="header" class="alt">
-      <h1><a href="../inicio.html">Clic And Eat</a> </h1>
-      <nav id="nav">
-        <ul>
-          <li><a href="../servicios.html"><i class="material-icons">headset_mic</i> Servicios</a></li>
-          <li><a href="../platillos/read.html"><i class="material-icons">restaurant_menu</i>Menu de platillos</a></li>
-          <li><a href="../pedidos.html"> <i class="material-icons">room_service</i> Pedidos</a></li>
-          <li><a href="../perfil/read.html"> <i class="material-icons">face</i>Perfil</a></li>
-          <li><a href="../../index.html" class="button">Cerrar Sesión</a></li>
-        </ul>
-      </nav>
-    </header>
-    <form action="../../controller/perfil/editarperfil.php?idrestaurante=<?php echo $idrestaurante?>" method="POST" enctype="multipart/form-data">>
-      <section id="banner">
-        <img src="http://s3-us-west-1.amazonaws.com/facilite/V2/files/objeto/logo/80/59/691386804555/WINGS_ARMY_logo.jpg" alt=""
-        style="height: 300px; width: 300px;
-        background-repeat: no-repeat;
-        background-position: 50%;
-        border-radius: 80%;
-        background-size: 100% 100%;">
-        <input type="file" name="file" id="file" class="inputfile" style="width: 0.1px;
-        height: 0.1px;
-        opacity: 0;
-        overflow: hidden;
-        position: absolute;
-        z-index: -1;" />
-         
-        <section id="main" class="container 75%">
-          <div class="box">
-            <div class="12u">
-         
-               <div class="input-field col s6">
-        <input name="foto" id="imagen" type="file" class="validate" class="filestyle" data-input="false">
-        </div>
-     <br>
-              <input id="" name="nombrerest" type="text" value="<?php echo $row['nombrerest'];?>" placeholder="Nombre del restaurante">
+<nav class="z-depth-2 #00695c teal darken-3" role="navigation">
+  <div class="nav-wrapper container">
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="../servicios.php"><i class="material-icons left">headset_mic</i>Servicios</a></li>
+      <li><a href="../platillos/read.php"><i class="material-icons left">restaurant_menu</i>Menu de platillos</a></li>
+      <li><a href="../pedidos.php"><i class="material-icons left">room_service</i>Pedidos</a></li>
+      <li><a href="read.php"><i class="material-icons left">face</i>Perfil</a></li>
+      <li><a href="../../index.html"><i class="material-icons right">directions_run</i>Cerrar sesión</a></li>
+    </ul>
+  </div>
+</nav>
+<br><br><br>
+<body>
+  <form action="../../controller/perfil/editarperfil.php?idrestaurante=<?php echo $idrestaurante?>" method="POST" enctype="multipart/form-data">
+    <div class="center-align">
+      <?php
+      $ruta = "../../controller/perfil/imagenes/" . $row['logo'];
+      ?>
+      <img src="<?php echo $ruta?>" class="circle" style=" height: 300px;
+      width: 300px;
+      background-repeat: no-repeat;
+      background-position: 50%;
+      border-radius: 80%;
+      background-size: 100% 100%">
+      <div class="row">
+        <div class="col s8 offset-s2">
+          <div class="card ">
+            <div class="file-field input-field">
+              <div class="btn">
+                <span>Subir foto</span>
+                <input name="foto" id="imagen" type="file" class="validate" class="btn" data-input="false">
+              </div>
+              <div class="file-path-wrapper">
+                <input class="file-path validate" type="text">
+              </div>
             </div>
-            <div class="12u">
-              ""
-              <input id="" name="slogan" type="text"  value="<?php echo $row['slogan'];?>" placeholder="Slogan">
+            <div class="input-field">
+              <input id="nombre"type="text" style="color: Green" value="<?php echo $row['nombrerest'];?>"/>
+              <label class="active"for="nombre">Nombre de restaurte:</label>
             </div>
-            <br>
-            <div class="12u">
-              <textarea id="" name="descripcion"  placeholder="Descripcion"><?php echo $row['descripcion'];?></textarea>
-              <br>
+            <div class="input-field">
+              <input id="slogan"type="text" style="color: Green" value="<?php echo $row['slogan'];?>"/>
+              <label class="active"for="slogan">Slogan:</label>
             </div>
-            <div class="12u">
-              <input  name="domicilio" type="text"  value="<?php echo $row['domicilio'];?>" placeholder="Domicilio completo">
+            <div class="input-field">
+              <input id="descripcion"type="text" style="color: Green" value="<?php echo $row['descripcion'];?>"/>
+              <label class="active"for="descripcion">Descripcion:</label>
             </div>
-            <div class="12u">
-              ""
-              <input id="" name="telefono" type="text"  value="<?php echo $row['telefono'];?>" placeholder="Telefono">
+            <div class="input-field">
+              <input id="dir"type="text" style="color: Black" value="<?php echo $row['domicilio'];?>"/>
+              <label class="active"for="dir">Domicilio:</label>
             </div>
-            <br>
-            <div class="12u">
-              <input id="" name="horarios" type="text"  value="<?php echo $row['horarios'];?>" placeholder="Horarios">
+            <div class="input-field">
+              <input id="telefono"type="text" style="color: Black" value="<?php echo $row['telefono'];?>"/>
+              <label class="active"for="telefono">Telefono:</label>
+            </div>
+            <div class="input-field">
+              <input id="horarios"type="text" style="color: Black" value="<?php echo $row['horarios'];?>"/>
+              <label class="active"for="horarios">Horarios:</label>
             </div>
           </div>
           <div class="12u" style="text-align: right;" >
             <ul class="actions">
               <li>
-                <button type="submit" style="background: gray; display:scroll; position:fixed; bottom:50px; right:35px;">Guardar Cambios</button>
               </li>
             </ul>
           </div>
         </div>
+        <div class="12u" style="text-align: right;" >
+          <ul class="actions">
+            <li>
+              <button class="btn-floating btn-large waves-effect waves-light"type="submit"
+              style="background: gray; display:scroll; position:fixed; bottom:50px; right:35px;">
+              <i class="material-icons">save</i></button>
+            </li>
+          </ul>
+        </div>
+      </form>
+      <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
 
-
-      </section>
-    </form>
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
-
-    <script type="text/javascript">
+      <script type="text/javascript">
       function initialize() {
 
         var input = document.getElementById('dir');
@@ -101,6 +112,6 @@ $row=mysql_fetch_array($sql);
       }
 
       google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
-  </body>
-  </html>
+      </script>
+    </body>
+    </html>
